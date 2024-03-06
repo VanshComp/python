d7348ea7abb0b284c5b3af956bba2780c0a2cf82
VanshComp <vanshgautam2005@gmail.com>
2024-03-06 18:29:22 +0530

    Loops

---
+++ loops.py
@@ -0,0 +1,64 @@
+######################  WHILE L00P ###################################################
+# Print numbers from 1 to 100.
+
+# count=1
+# while count<=100:
+#     print(count)
+#     count+=1
+
+# Print the elements of the following list using a loop:
+# [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+
+# count=1
+# while count<=10:
+#     print(count*count)
+#     count+=1
+# Print numbers from 100 to 1.
+
+# count=100
+# while count >= 1:
+#     print(count)
+#     count -= 1
+
+# Print the multiplication table of a number n.
+
+# n=int(input("Enter any number: "))
+# count=1
+# while count<=10:
+#     print("%d * %d = %d" % (n,count,n*count))
+#     count+=1
+
+# Search for a number x in this tuple using loop:
+# [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+
+# tup=[1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+# x=int(input('Enter any number from above: '))
+# i=0
+# while i<len(tup):
+#     if tup[i]==x:
+#         print("%d is found" % (x))
+#         break
+#     i=i+ 1    
+
+# Print the elements of the following list using a loop:
+# [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+
+# list=input("Enter list to be printed: ")
+# my_list=list.split()
+# for val in my_list:
+#     print(val)
+
+# Search for a number x in this tuple using loop:
+# [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+
+# list_input = input("Enter list separated by spaces: ")
+# my_list = list_input.split()
+# x = input("Enter number you want to find from above: ")
+
+# for num in my_list:
+#     if num == x:
+#         print("%s is found" % x)
+#         break
+# else:
+#     print("%s is not in the above list" % x)
+    
